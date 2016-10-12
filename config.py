920c31bc2db39e0d347c8997d6d904003c2dbe1b
Sean <seanxu1015@gmail.com>
2016-10-12 11:04:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ class Config(object):
     embedding_size = 40
     memory_size = 50
     random_state = None
-    task_ids = [1, 7, 8, 11, 12, 13, 18, 20]
-    data_dir = 'data/tasks_1-20_v1-2/en/'
+    task_ids = range(1,21)
+    data_dir = 'tasks_1-20_v1-2/en/'
     output_file = 'results.csv'
     val_size = 0.1
     stddev = 0.1
     learning_rate = 1e-2
     name ='memory'
-    save_file = './weights/memory09'
-    load_file = './weights/memory09'
+    save_file = './weights'
+    load_file = None
